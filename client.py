cdd76633de358453438a0f5dabda52b57d0b2e3a
redfast00 <redfast00@gmail.com>
2022-02-08 22:05:20 +0100

    Get credentials from file in commandline client

--- client.py
+++ client.py
@@ -3,19 +3,20 @@ import time
 import requests
 import hashlib
 import hmac
-
+uplink = 'http://10.0.1.5/lockbot'
+#uplink = 'https://kelder.zeus.ugent.be/lockbot'
 
 command = sys.argv[1]
 if command == 'close':
     command = 'lock'
 
-t = int(time.time())
-down_key = 'DOWNKEY'
+with open('tokens.h') as credfile:
+    down_key = [line.split('"')[1] for line in credfile if 'DOWN_COMMAND_KEY' in line][0]
 
+t = int(time.time()) * 1000
 payload = f'{t};{command}'
 
 hash = hmac.new(down_key.encode('utf8'), payload.encode('utf8'), hashlib.sha256).hexdigest().upper()
 print(hash)
-r = requests.post('https://kelder.zeus.ugent.be/lockbot', payload, headers={'HMAC': hash})
-
+r = requests.post(uplink, payload, headers={'HMAC': hash})
 print(r.text)

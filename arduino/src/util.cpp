d8b2c7fd01da04dfe25bdfdb9621e071bf963d36
Jonas Meeuws <jonas.meeuws@gmail.com>
2025-11-04 00:46:28 +0100

    style(arduino): Add and apply editorconfig.

--- arduino/src/util.cpp
+++ arduino/src/util.cpp
@@ -1,25 +1,25 @@
 #include "util.h"
 
 bool Debounced::loop(bool currently_down) {
-	uint32_t now = millis();
-	if (currently_down) {
-		press_last_detected = now;
-		if (!pressing) {
-			// Start of new button press
-			pressing = true;
-			press_count++;
-			return true;
-		}
-	} else {
-		if (pressing && now > press_last_detected + debounce) {
-			pressing = false;
-		}
-	}
-	return false;
+  uint32_t now = millis();
+  if (currently_down) {
+    press_last_detected = now;
+    if (!pressing) {
+      // Start of new button press
+      pressing = true;
+      press_count++;
+      return true;
+    }
+  } else {
+    if (pressing && now > press_last_detected + debounce) {
+      pressing = false;
+    }
+  }
+  return false;
 }
 
 uint16_t Debounced::get_and_reset_press_count() {
-	uint16_t output = press_count;
-	press_count = 0;
-	return output;
+  uint16_t output = press_count;
+  press_count = 0;
+  return output;
 }
